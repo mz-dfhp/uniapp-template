f4d8ddaeae2b0e8af7edf35f3a76f8c2c962c1e5
lc <486836332@qq.com>
2024-02-20 14:07:58 +0800

    feat: static page

---
+++ src/pages/my/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <view class="content">
+        {{ title }}
+    </view>
+</template>
+  
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('my')
+</script>
+  
+<style scoped lang="scss"></style>
