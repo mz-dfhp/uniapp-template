865e171dcb5dab48c0fcef27945f43fe3de4ce16
lc <486836332@qq.com>
2024-02-20 14:21:09 +0800

    chore: add eslint

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('index')
+</script>
+
 <template>
   <view class="content">
     {{ title }}
   </view>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('index')
-</script>
 
 <style scoped lang="scss"></style>
--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,12 +1,13 @@
-<template>
-    <view class="content">
-        {{ title }}
-    </view>
-</template>
-  
 <script setup lang="ts">
 import { ref } from 'vue'
+
 const title = ref('my')
 </script>
-  
+
+<template>
+  <view class="content">
+    {{ title }}
+  </view>
+</template>
+
 <style scoped lang="scss"></style>
